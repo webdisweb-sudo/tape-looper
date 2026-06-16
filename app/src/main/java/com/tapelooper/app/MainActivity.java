87f2a5ada638e0fe3cf92fec447e07aaa7ae40b7
webdisweb-sudo <webdisweb@gmail.com>
2026-06-16 07:03:01 +0200

    update app/src/main/java/com/tapelooper/app/MainActivity.java

--- app/src/main/java/com/tapelooper/app/MainActivity.java
+++ app/src/main/java/com/tapelooper/app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.tapelooper.app;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.*;
 import android.content.pm.PackageManager;
@@ -10,7 +12,9 @@ import androidx.core.content.ContextCompat;
 
 public class MainActivity extends Activity {
     WebView webView;
+    ValueCallback<Uri[]> filePathCallback;
     static final int MIC_REQ = 1;
+    static final int FILE_REQ = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +28,28 @@ public class MainActivity extends Activity {
         s.setAllowFileAccessFromFileURLs(true);
         s.setAllowUniversalAccessFromFileURLs(true);
         s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
 
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onPermissionRequest(PermissionRequest request) {
                 request.grant(request.getResources());
+            }
+
+            @Override
+            public boolean onShowFileChooser(WebView webView,
+                    ValueCallback<Uri[]> filePathCallback,
+                    FileChooserParams fileChooserParams) {
+                if (MainActivity.this.filePathCallback != null) {
+                    MainActivity.this.filePathCallback.onReceiveValue(null);
+                }
+                MainActivity.this.filePathCallback = filePathCallback;
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("audio/*");
+                startActivityForResult(Intent.createChooser(intent, "Выберите аудиофайл"), FILE_REQ);
+                return true;
             }
         });
 
@@ -39,6 +60,22 @@ public class MainActivity extends Activity {
         }
 
         webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILE_REQ) {
+            if (filePathCallback != null) {
+                Uri[] results = null;
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    Uri uri = data.getData();
+                    if (uri != null) results = new Uri[]{uri};
+                }
+                filePathCallback.onReceiveValue(results);
+                filePathCallback = null;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
